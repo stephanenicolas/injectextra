be4f8dc0c9db3bb2225dc00bbedc431530479808
Stéphane Nicolas <steff.nicolas@gmail.com>
2014-08-26 12:17:01 -0400

    Compiles and should more or less work

--- injectextra-annotations/src/main/java/com/github/stephanenicolas/injectextra/InjectExtra.java
+++ injectextra-annotations/src/main/java/com/github/stephanenicolas/injectextra/InjectExtra.java
@@ -15,31 +15,40 @@
  */
 package com.github.stephanenicolas.injectextra;
 
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
-
 /**
  * Indicates that a variable member of a class (whether static or not) should be
- * injected with an Android extra.
+ * injected with an Android extra. The value is compulsory, and corresponds to
+ * the key of the extra.<br />
+ * <br />
+ * The extra must exist when the activity is injected, unless you specify
+ * {@code optional=true} in the {@link InjectExtra} annotation. If optional is
+ * set to true and no extra is found, no value will be injected in the field.<br />
+ * <br />
+ * The default behavior of the {@link InjectExtra} annotation is to forbid null
+ * values. However, if you wish to allow injection of null values, you should
+ * use the TODO XXX Nullable XXX annotation.<br />
+ * <br />
+ * You can define a default value in Java when the extra is optional : <br /> {@code
+ * @InjectExtra(value="someValue", optional=true) Integer someValue = 2;} ) <br />
+ * However, it is nonsense to inject a default value in Java if the extra is
+ * not optional : {@code @InjectExtra("someValue") Integer someValue = 2; // DO
+ * NOT DO THIS}
  *
- * The value corresponds to the id of the extra.<br />
- *
- * You may specify the name of the extra instead of the id using {@link #name()},
- * which will use {@link android.content.res.Extras#getIdentifier(String, String, String)} to
- * resolve the extra by name.
- *
- * Usage example:<br />
- * {@code @InjectExtra(R.string.hello) protected String hello;} <br/>
- * {@code @InjectExtra(name="com.myapp:string/hello") protected String hello;}
+ * <br />
+ * Usage example:<br /> {@code @InjectExtra("someValue") protected Integer someValue;}<br />
+ * <br />{@code @InjectExtra("someValue") @Nullable protected Integer someValue;}
  *
  * @author Mike Burton
  */
-@Retention(RUNTIME) @Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
+@Retention(RUNTIME)
+@Target( { ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
 public @interface InjectExtra {
-  int value() default -1;
-
-  String name() default "";
+  String value();
+  boolean optional() default false;
 }
--- injectextra-plugin/src/main/java/com/github/stephanenicolas/injectextra/InjectExtraProcessor.java
+++ injectextra-plugin/src/main/java/com/github/stephanenicolas/injectextra/InjectExtraProcessor.java
@@ -1,31 +1,16 @@
 package com.github.stephanenicolas.injectextra;
 
 import android.app.Activity;
-import android.app.Application;
-import android.app.Fragment;
-import android.app.Service;
-import android.content.BroadcastReceiver;
-import android.content.ContentProvider;
-import android.content.Context;
-import android.content.res.ColorStateList;
-import android.graphics.Movie;
-import android.graphics.drawable.Drawable;
-import android.view.View;
-import android.view.animation.Animation;
 import com.github.stephanenicolas.afterburner.AfterBurner;
 import com.github.stephanenicolas.afterburner.exception.AfterBurnerImpossibleException;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.regex.Matcher;
 import javassist.CannotCompileException;
 import javassist.ClassPool;
 import javassist.CtClass;
-import javassist.CtConstructor;
 import javassist.CtField;
-import javassist.CtMethod;
 import javassist.NotFoundException;
 import javassist.build.IClassTransformer;
 import javassist.build.JavassistBuildException;
@@ -34,67 +19,169 @@ import lombok.extern.slf4j.Slf4j;
 import static java.lang.String.format;
 
 /**
- * Will inject all extras in the following supported classes :
- * <ul>
- * <li>{@link Activity}</li>
- * <li>{@link Service}</li>
- * <li>{@link Application}</li>
- * <li>{@link Fragment}</li>
- * <li>support Fragment</li>
- * <li>{@link View}</li>
- * <li>{@link ContentProvider}</li>
- * <li>and any other class that defines a constructor with an argument
- * that is in the list above</li>
- * <li>{@link BroadcastReceiver}</li>
- * </ul>
- *
- * <pre>
- * <ul>
- *   <li>for activities, services and applications:
- *     <ul>
- *       <li>right after super.onCreate in onCreate
- *     </ul>
- *   <li>for contentProviders:
- *     <ul>
- *       <li>at the beginning of onCreate
- *     </ul>
- *   <li>for broadcastReceivers:
- *     <ul>
- *       <li>at the beginning of onReceive
- *     </ul>
- *   <li>for fragments :
- *     <ul>
- *       <li>right after super.onAttach in onAttach
- *     </ul>
- *   <li>for views :
- *     <ul>
- *       <li>right after super.onFinishInflate in onFinishInflate
- *       <li>onFinishInflate is called automatically by Android when inflating a view from XML
- *       <li>onFinishInflate must be called manually in constructors of views with a single context
- * argument. You should invoke it after inflating your layout manually.
- *     </ul>
- *   <li>for other classes (namely MVP presenters and view holder design patterns) :
- *     <ul>
- *       <li>right before any constructor with an argument of a supported type
- *       (except BroadcastReceivers)
- *       <li>inner classes can only be processed if they are static
- *     </ul>
- * </ul>
- * </pre>
+ * TODO
  *
  * @author SNI
  */
 @Slf4j
 public class InjectExtraProcessor implements IClassTransformer {
 
+  private final static String GET_APPLICATION_TAG = "GET_APPLICATION_TAG";
+  private static final String GET_ROOT_TAG = "GET_ROOT";
+
+  private AfterBurner afterBurner = new AfterBurner();
+
   @Override
   public boolean shouldTransform(CtClass candidateClass) throws JavassistBuildException {
-  return false;
+    try {
+      final List<CtField> fields =
+          getAllInjectedFieldsForAnnotation(candidateClass, InjectExtra.class);
+
+      if (fields.isEmpty()) {
+        return false;
+      }
+
+      boolean isValid = isValidClass(candidateClass);
+
+      log.debug(
+          format("Class %s will get transformed: %b", candidateClass.getSimpleName(), isValid));
+      return isValid;
+    } catch (Exception e) {
+      String message = format("Error while filtering class %s", candidateClass.getName());
+      log.debug(message, e);
+      throw new JavassistBuildException(message, e);
+    }
   }
 
   @Override
   public void applyTransformations(final CtClass classToTransform) throws JavassistBuildException {
+    // Actually you must test if it exists, but it's just an example...
+    log.debug("Analyzing " + classToTransform.getSimpleName());
+
+    try {
+      final List<CtField> fields =
+          getAllInjectedFieldsForAnnotation(classToTransform, InjectExtra.class);
+      String injectExtraStatements = createInjectExtraStatements(fields, classToTransform);
+
+      log.debug("Preliminary body (before insertion) :" + injectExtraStatements);
+      injectStuff(classToTransform, injectExtraStatements);
+    } catch (Exception e) {
+      String message = format("Error while processing class %s", classToTransform.getName());
+      log.debug(message, e);
+      throw new JavassistBuildException(message, e);
+    }
   }
 
+  //extension point for new classes
+  private void injectStuff(CtClass targetClazz, String body)
+      throws CannotCompileException, AfterBurnerImpossibleException, NotFoundException,
+      InjectExtraException {
+    afterBurner.afterOverrideMethod(targetClazz, "onCreate", body);
+  }
 
+  private List<CtField> getAllInjectedFieldsForAnnotation(CtClass clazz,
+      Class<? extends Annotation> annotationClazz) {
+    List<CtField> result = new ArrayList<CtField>();
+    CtField[] allFields = clazz.getDeclaredFields();
+    log.debug("Scanning fields in " + clazz.getName());
+    for (CtField field : allFields) {
+      log.debug("Discovered field " + field.getName());
+      if (field.hasAnnotation(annotationClazz)) {
+        log.debug(
+            "Field " + field.getName() + " has annotation " + annotationClazz.getSimpleName());
+        result.add(field);
+      }
+    }
+    return result;
+  }
+
+  private String createInjectExtraStatements(List<CtField> extrasToInject, CtClass targetClazz)
+      throws ClassNotFoundException, NotFoundException {
+    StringBuffer buffer = new StringBuffer();
+    for (CtField field : extrasToInject) {
+      Object annotation = field.getAnnotation(InjectExtra.class);
+      //must be accessed by introspection as I get a Proxy during tests.
+      //this proxy comes from Robolectric
+      Class annotionClass = annotation.getClass();
+
+      //workaround for robolectric
+      //https://github.com/robolectric/robolectric/pull/1240
+      int id = 0;
+      boolean tag = false;
+      try {
+        Method method = annotionClass.getMethod("value");
+        id = (Integer) method.invoke(annotation);
+        method = annotionClass.getMethod("optional");
+        tag = (boolean) method.invoke(annotation);
+      } catch (Exception e) {
+        throw new RuntimeException("How can we get here ?");
+      }
+      boolean isUsingId = id != -1;
+
+      buffer.append(field.getName());
+      buffer.append(" = ");
+
+      String root = "$1";
+      String findExtraString = "";
+      ClassPool classPool = targetClazz.getClassPool();
+      if (isSubClass(classPool, field.getType(), String.class)) {
+        findExtraString = "getString(" + id + ")";
+      } else if (field.getType().subtypeOf(CtClass.booleanType)) {
+        findExtraString = "getBoolean(" + id + ")";
+      } else if (isSubClass(classPool, field.getType(), Boolean.class)) {
+        root = null;
+        findExtraString = "new Boolean($1.getBoolean(" + id + "))";
+      } else if (field.getType().subtypeOf(CtClass.intType)) {
+        findExtraString = "getInt(" + id + ")";
+      } else if (isSubClass(classPool, field.getType(), Integer.class)) {
+        root = null;
+        findExtraString = "new Integer($1.getInt(" + id + "))";
+      } else if (isStringArray(field, classPool)) {
+        findExtraString = "getStringArray(" + id + ")";
+      } else if (isIntArray(field)) {
+        findExtraString = "getIntArray(" + id + ")";
+      } else {
+        throw new NotFoundException(
+            format("InjectExtra doen't know how to inject field %s of type %s in %s",
+                field.getName(), field.getType().getName(), targetClazz.getName()));
+      }
+      if (root != null) {
+        buffer.append(root);
+        buffer.append(".");
+      }
+      buffer.append(findExtraString);
+      buffer.append(";\n");
+    }
+    return buffer.toString();
+  }
+
+  private boolean isStringArray(CtField field, ClassPool classPool) throws NotFoundException {
+    return field.getType().isArray() && isSubClass(classPool, field.getType().getComponentType(),
+        String.class);
+  }
+
+  private boolean isIntArray(CtField field) throws NotFoundException {
+    return field.getType().isArray() && field.getType()
+        .getComponentType()
+        .subtypeOf(CtClass.intType);
+  }
+
+  //extension point for new classes
+  private boolean isValidClass(CtClass clazz) {
+    return isActivity(clazz);
+  }
+
+  private boolean isActivity(CtClass clazz) {
+    try {
+      return isSubClass(clazz.getClassPool(), clazz, Activity.class);
+    } catch (NotFoundException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private boolean isSubClass(ClassPool classPool, CtClass clazz, Class<?> superClass)
+      throws NotFoundException {
+    CtClass superclass = classPool.get(superClass.getName());
+    return clazz.subclassOf(superclass);
+  }
 }
